0ffcd906ae5b200a82070938df7ed8f498732d54
Camilo Castro <camilocastro@worldoffice.com.co>
2024-01-08 20:46:47 -0500

    add author

--- src/main/java/masiv/com/prueba/ElevatorApplication.java
+++ src/main/java/masiv/com/prueba/ElevatorApplication.java
@@ -3,6 +3,10 @@ package masiv.com.prueba;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+/**
+ * Author: Camilo Andres Castro Mesa
+ * Date: 09/01/2024
+ */
 @SpringBootApplication
 public class ElevatorApplication {
 
