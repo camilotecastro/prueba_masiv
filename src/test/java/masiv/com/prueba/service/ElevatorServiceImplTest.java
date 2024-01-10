5d9fc449cc6cd4662d283d72dc369d41abc16170
Camilo Castro <camilocastro@worldoffice.com.co>
2024-01-09 23:47:26 -0500

    test: se agregan nuevos tests.

--- src/test/java/masiv/com/prueba/service/ElevatorServiceImplTest.java
+++ src/test/java/masiv/com/prueba/service/ElevatorServiceImplTest.java
@@ -61,6 +61,7 @@ class ElevatorServiceImplTest {
     @Test
     void testRequestMoveAndGetStateElevator() {
 
+        // 10 -> 1 -> 0 -> 10 -> 0 -> 10  UP
         assertEquals("", elevatorServiceImpl.requestElevator(10, true));
         assertEquals("It is not possible to add a floor to the queue that is equal to the current floor where the elevator"
                 + " is located.", elevatorServiceImpl.requestElevator(1, true));
@@ -76,6 +77,82 @@ class ElevatorServiceImplTest {
         assertTrue(actualElevatorState.getPendingFloorsOutsideElevator().isEmpty());
 
     }
+
+    /**
+     * Method under test: {@link ElevatorServiceImpl#moveElevator()}
+     */
+
+    @Test
+    void testRequestMoveAndGetStateElevator2() {
+
+        assertEquals("", elevatorServiceImpl.requestElevator(3, true));
+        elevatorServiceImpl.moveElevator();
+
+        // 3 -> 8 -> 10 -> 6 -> 4 -> 2 DOWN
+        assertEquals("", elevatorServiceImpl.requestElevator(8, true));
+        assertEquals("", elevatorServiceImpl.requestElevator(10, false));
+        assertEquals("", elevatorServiceImpl.requestElevator(6, false));
+        assertEquals("", elevatorServiceImpl.requestElevator(4, false));
+        assertEquals("", elevatorServiceImpl.requestElevator(2, false));
+
+        elevatorServiceImpl.moveElevator();
+        ElevatorState actualElevatorState = elevatorServiceImpl.getElevatorState();
+        assertEquals(2, actualElevatorState.getCurrentFloor());
+        assertTrue(actualElevatorState.getPendingFloorsInsideElevator().isEmpty());
+        assertTrue(actualElevatorState.getPendingFloorsOutsideElevator().isEmpty());
+
+    }
+
+    /**
+     * Method under test: {@link ElevatorServiceImpl#moveElevator()}
+     */
+    @Test
+    void testRequestMoveAndGetStateElevator3() {
+
+        assertEquals("", elevatorServiceImpl.requestElevator(10, true));
+        elevatorServiceImpl.moveElevator();
+
+        // 10  -> 9 -> 6 -> 2 -> 4 -> 7 -> 11 UP
+        assertEquals("", elevatorServiceImpl.requestElevator(9, true));
+        assertEquals("", elevatorServiceImpl.requestElevator(6, true));
+        assertEquals("", elevatorServiceImpl.requestElevator(2, true));
+        assertEquals("", elevatorServiceImpl.requestElevator(11, false));
+        assertEquals("", elevatorServiceImpl.requestElevator(4, false));
+        assertEquals("", elevatorServiceImpl.requestElevator(7, false));
+
+        elevatorServiceImpl.moveElevator();
+        ElevatorState actualElevatorState = elevatorServiceImpl.getElevatorState();
+        assertEquals(11, actualElevatorState.getCurrentFloor());
+        assertTrue(actualElevatorState.getPendingFloorsInsideElevator().isEmpty());
+        assertTrue(actualElevatorState.getPendingFloorsOutsideElevator().isEmpty());
+
+    }
+
+    /**
+     * Method under test: {@link ElevatorServiceImpl#moveElevator()}
+     */
+    @Test
+    void testRequestMoveAndGetStateElevator4() {
+
+        assertEquals("", elevatorServiceImpl.requestElevator(4, true));
+        elevatorServiceImpl.moveElevator();
+
+        // 4 -> 5 -> 9 -> 12 -> 3 -> 2
+        assertEquals("", elevatorServiceImpl.requestElevator(9, true));
+        assertEquals("", elevatorServiceImpl.requestElevator(5, true));
+        assertEquals("", elevatorServiceImpl.requestElevator(12, true));
+        assertEquals("", elevatorServiceImpl.requestElevator(3, false));
+        assertEquals("", elevatorServiceImpl.requestElevator(2, false));
+        assertEquals("It is not possible to add a floor to the queue that is equal to the current floor where the elevator is located.",
+                elevatorServiceImpl.requestElevator(4, false));
+        elevatorServiceImpl.moveElevator();
+        ElevatorState actualElevatorState = elevatorServiceImpl.getElevatorState();
+        assertEquals(2, actualElevatorState.getCurrentFloor());
+        assertTrue(actualElevatorState.getPendingFloorsInsideElevator().isEmpty());
+        assertTrue(actualElevatorState.getPendingFloorsOutsideElevator().isEmpty());
+
+    }
+
 
 
 }
